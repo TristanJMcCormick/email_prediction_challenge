b22b0b2933a1710d5b406b5023401cb71117bf8d
tristanjmccormick <tristanjmccormick@gmail.com>
2014-11-26 11:51:49 -0600

    Sketched out email class, making module

---
+++ Email_Object_Module.rb
@@ -0,0 +1,37 @@
+class EmailObject
+  attr_accessor
+  :name
+  :first_name
+  :last_name
+  :first_local_part
+  :last_local_part
+  :domain
+  def initialize(name, email)
+    @name = name
+    determine_name_parts(name)
+    determine_email_parts(email)
+    determine_email_format
+  end
+  def determine_name_parts(name)
+    @first_name = name[0..name.index(" ")].strip;
+    @last_name = name[name.index(" ")..name.length].strip;
+  end
+  def determine_email_parts(email)
+    @first_local_part = email[0..email.index(".") - 1].strip;
+    @last_local_part = email[email.index(".") + 1..email.index("@") - 1].strip;
+    @domain = email[email.index("@") + 1..email.length].strip;
+  end
+  def determine_email_format
+    if @first_name == @first_local_part && @last_name == @last_local_part
+      @email_form = "first_initial_dot_last_initial"
+    elsif @first_name[0] == @first_local_part && @last_name[0] == @last_local_part
+      @email_form = "first_initial_dot_last_initial"
+    elsif @first_name[0] == @first_local_part && @last_name == @last_local_part
+      @email_form = "first_initial_dot_last_name"
+    elsif @first_name == @first_local_part && @last_name[0] == @last_local_part
+      @email_form = "first_name_dot_last_initial"
+    else
+      @email_form = "unclear"
+    end
+  end
+end
--- Tristan_McCormick_Alphasights_exercise.rb
+++ Tristan_McCormick_Alphasights_exercise.rb
@@ -1,7 +1,28 @@
-#Takes an array of name-emails
-#parses them into name:email format pairs
-#
-#
-#
-#
-#
+#Takes two hashes, one of name-emails (sample dataset) and another of
+#name-company pairs
+#parses the first array into first name, last name, local part and domain
+#Calculate email format
+#Inserts company name:email format pairs into dictionary--
+#--Where the email format is one of the four offered
+#Take second array of names and companies
+#If company is in dictionary, calculate possible email--
+#--according to associated pattern
+#If not, generate all four with error message.
+
+
+sample_dataset = {
+  "John Ferguson" => "john.ferguson@alphasights.com",
+  "Damon Aw" => "damon.aw@alphasights.com",
+  "Linda Li" => "linda.li@alphasights.com",
+  "Larry Page" => "larry.p@google.com",
+  "Sergey Brin" => "s.brin@google.com",
+  "Steve Jobs" => "s.j@apple.com"
+}
+
+email_objects_hash = Hash.new(sample_dataset.length)
+
+sample_dataset.each do |name, email|
+  s = EmailObject.new(name,email)
+  email_objects_hash[s.name] = s
+end
+puts email_objects_hash
